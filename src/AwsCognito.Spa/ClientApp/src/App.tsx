b36f6ca0a9bc2f56e638ef876618686840bd81cf
Mauro Bertoli <info@stellarsolutions.it>
2021-05-07 13:16:16 +0200

    add react Cognito authentication

--- src/AwsCognito.Spa/ClientApp/src/App.tsx
+++ src/AwsCognito.Spa/ClientApp/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { withAuthenticator, AmplifySignOut, AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
 import './App.css';
 import axios from 'axios';
 
@@ -19,18 +20,55 @@ function App() {
 
 
 	return (
-		<div className="App">
-			{obj?.map(o => (
-				<>
-					<p>
-						Date: {new Date(o.date).toLocaleDateString()},
-						C: {o.temperatureC},
-						F: {o.temperatureF},
-						Summary: {o.summary}
-					</p>
-				</>
-			))}
-		</div>
+		<>
+			<AmplifyAuthenticator>
+				<AmplifySignUp
+					slot="sign-up"
+					usernameAlias="email"
+					formFields={[
+						{
+							type: "email",
+							label: "Email",
+							placeholder: "Type here your email",
+							required: true,
+						},
+						{
+							type: "password",
+							label: "Password",
+							placeholder: "Type here your password",
+							required: true,
+						},
+						// {
+						// 	type: "phone_number",
+						// 	label: "Custom Phone Label",
+						// 	placeholder: "custom Phone placeholder",
+						// 	required: true,
+						// },
+						{
+							type: "name",
+							label: "Your fullname",
+							placeholder: "Type here your fullname",
+							required: true,
+						},
+					]}
+				/>
+
+				<AmplifySignOut />
+
+				<div className="App">
+					{obj?.map(o => (
+						<>
+							<p>
+								Date: {new Date(o.date).toLocaleDateString()},
+								C: {o.temperatureC},
+								F: {o.temperatureF},
+								Summary: {o.summary}
+							</p>
+						</>
+					))}
+				</div>
+			</AmplifyAuthenticator>
+		</>
 	);
 }
 
